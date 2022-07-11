3a485bbda2c0cb4236ea5ddca5a841572cdd7983
ws4u <105423832+Wswolf1@users.noreply.github.com>
2022-07-11 15:50:04 +0700

    Update hacker.js

--- plugins/hacker.js
+++ plugins/hacker.js
@@ -3,9 +3,9 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix }) => {
   conn.reply(m.chat, `*${pickRandom(hacker)}*`, m)
 }
-handler.help = ['katakanna']
+handler.help = ['kataNayla']
 handler.tags = ['quotes']
-handler.command = /^(katakanna)$/i
+handler.command = /^(katanayla)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
